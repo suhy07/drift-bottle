a8a9ea283547423f659a9fe0e1aef583b3e65442
suhy <704722924@qq.com>
2021-10-28 20:33:59 +0800

    10.28

--- app/src/main/java/com/example/myhomework/MemObject/Msg.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myhomework.MemObject
-
-class Msg(val content:String,val type:Int){
-    //定义静态成员
-    companion object{
-        const val RIGHT = 0
-        const val LEFT = 1
-    }
-};
--- app/src/main/java/com/example/myhomework/MemObject/UserHead.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myhomework.MemObject
-
-import android.net.Uri
-
-class UserHead(val userHeadUri:Int,val id:String,val historyMsg:String,val msgNum:Int) {
-
-}
