463813de93dfd53498173e9c48ce6f81a9df3eb9
suhy <704722924@qq.com>
2022-05-21 21:25:26 +0800

    temp

--- MyHomeWork/app/src/main/java/com/example/myhomework/activity/MainActivity.java
+++ MyHomeWork/app/src/main/java/com/example/myhomework/activity/MainActivity.java
@@ -19,36 +19,17 @@ import com.example.myhomework.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static DrawerLayout drawerLayout;
     private ActivityMainBinding binding;
 
-// 调用类实现OnGetDataResultListener监听，用于接收Android AR识别SDK回调数据信息
-    /**
-     * 返回景区数据，跳转到景区Activity
-     * @param arSceneryResponse
-     */
-
-    public static MainActivity mainActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         hideActionBar();
-//        mainActivity=this;
-//        UserService.ResetUserData();
-//        drawerLayout=binding.drawerLayout;
-//        AppBarConfiguration mAppBarConfiguration = new AppBarConfiguration.Builder(
-//                R.id.page_news, R.id.page_photo, R.id.page_chat,R.id.page_home)
-//                .setOpenableLayout(drawerLayout)
-//                .build();
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-//        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-//        NavigationUI.setupWithNavController(binding.navView, navController);
-//        //binding.navigationView.setCo
+
         AppBarConfiguration mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.page_map,R.id.page_mine)
-                .setOpenableLayout(drawerLayout)
                 .build();
         PermissionsUtils.verifyStoragePermissions(this);
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
